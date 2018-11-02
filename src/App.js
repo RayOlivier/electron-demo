9b58d1db02de0ad3a510f4f0f18f42d005d46762
Ray Olivier <rayolivier@outlook.com>
2018-11-02 15:58:17 -0500

    custom menu and tray

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+// const { ipcRenderer } = window.require("electron")
+//this works in electron but not chrome. it's part of how you communicate between the front and back end
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      num: 0
+    }
+
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+  }
+
+  increment() {
+    this.setState({ num: this.state.num + 1 })
+  }
+
+  decrement() {
+    this.setState({ num: this.state.num - 1 })
+  }
+
   render() {
+    // console.log(ipcRenderer)
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+          <p>Electron Demo</p>
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
+          Num: {this.state.num}
+          <button onClick={this.increment}>Plus 1</button>{" "}
+          <button onClick={this.decrement}>Minus 1</button>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
